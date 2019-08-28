596d32cdc3b42f1987d8223c902eac0df4fda762
Sergey Sukhanov <sssuhanov@gmail.com>
2019-08-28 16:56:47 +0300

    face detector with face landmarks v0.02

--- face_detector.py
+++ face_detector.py
@@ -1,45 +1,102 @@
-# import libraries
-import cv2
+import cv2 as cv
 import face_recognition
- 
-# Get a reference to webcam
-video_capture = cv2.VideoCapture("/dev/video0")
- 
-# Initialize variables
+import numpy as np
+import time
+
+# Указываем какой камерой будем пользоваться
+cap = cv.VideoCapture("/dev/video0")
+
+if not cap.isOpened():
+    print("Не удалось подключиться к камере") 
+    exit()
+
+# Подготавливаем переменную для хранения областей лиц
 face_locations = []
- 
+# Подготавливаем переменную для хранения 
+face_landmarks_list = []
+
+# Параметры для контраста и яркости
+alpha = 1.5 # Simple contrast control
+beta = 50    # Simple brightness control
+
+# # Initialize values
+# print(' Basic Linear Transforms ')
+# print('-------------------------')
+# try:
+#     alpha = float(input('* Enter the alpha value [1.0-3.0]: '))
+#     beta = int(input('* Enter the beta value [0-100]: '))
+# except ValueError:
+#     print('Error, not a number')
+
+
 while True:
 	
-    #    Grab a single frame of video
-        
-    ret, frame = video_capture.read()
-    
-        
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        
-    rgb_frame = frame[:, :, ::-1]
-    
-        
-    # Find all the faces in the current frame of video
-        
-    face_locations = face_recognition.face_locations(rgb_frame)
-    
-        
-    # Display the results
-        
-    for top, right, bottom, left in face_locations:
-        # Draw a box around the face   	
-        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-        
-            
-    # Display the resulting image
-        
-    cv2.imshow('Video', frame)   
-    
-    # Hit 'q' on the keyboard to quit! 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # -----
+    # Берем кадр с камеры
+    # -----
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Не удается получить кадр")
         break
+
+    # -----
+    # Изменяем контраст и яркость
+    # -----
+    new_frame = np.zeros(frame.shape, frame.dtype)
+    result = cv.addWeighted(frame, alpha, new_frame, 0, beta)
+
+    # -----
+    # Переводим формат картинки из BGR в RGB так как его использует face_recognition
+    # -----
+    rgb_frame = frame[:, :, ::-1]
+            
+    # Находим лица на кадре с помощью face_locations
+    face_locations = face_recognition.face_locations(rgb_frame)
+            
+    # Наносим на кадр прямоугольники областей где обнаружены лица
+    # for top, right, bottom, left in face_locations:
+    #     # Рисуем прямоугольники вокруг лиц
+    #     cv.rectangle(result, (left, top), (right, bottom), (0, 0, 255), 2)
+    #     font = cv.FONT_HERSHEY_SIMPLEX
+    #     cv.putText(result, 'Face', (left, top-5), font, 1, (0, 0, 255))
+    
+    # -----
+    # Находим фичи лица на кадре
+    # -----
+    face_landmarks_list = face_recognition.face_landmarks(result)
+
+    # Выводим информацию о фичах лица в консоль
+    for face_landmarks in face_landmarks_list:
+
+        # Выводим в консоль обнаруженные фичи лица
+        # for facial_feature in face_landmarks.keys():
+        #     print("The {} in this face has the following points: {}".format(facial_feature, face_landmarks[facial_feature]))
+
+        # Рисуем полигоны вокруг обноруженных фич лица
+        for facial_feature in face_landmarks.keys():
+            pts = np.array(face_landmarks[facial_feature], np.int32)
+            pts = pts.reshape((-1,1,2))
+            cv.polylines(result, [pts], True, (0,255,255))
+
+            # Делаем подпись крайнего полигона
+            font = cv.FONT_HERSHEY_SIMPLEX
+            cv.putText(result, facial_feature, ([pts][0][0][0][0],[pts][0][0][0][1]), font, 0.5, (0, 0, 255))
+            
         
-# Release handle to the webcam
-video_capture.release()
-cv2.destroyAllWindows()
+    # пробую рисовать полигон
+    
+    # pts=np.array([[10,5],[20,30],[70,20],[50,10]], np.int32)
+    # pts = pts.reshape((-1,1,2))
+    # cv.polylines(result, [pts], True, (0, 255, 255))
+        
+    # Выводим кадр
+    cv.imshow('Video', result)
+    
+    # Добавляем возможность остановить выведение по нажатию кнопки 'q'
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Отключаемся от камеры и закрываем все окна
+cap.release()
+cv.destroyAllWindows()
